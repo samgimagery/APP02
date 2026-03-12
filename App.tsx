748c0bd27455f1a7678a0e5f716943d07e57c86e
Samuel Gaudreau <samg@Samuels-MacBook.local>
2026-03-12 19:12:17 +1100

    Update splash title

--- App.tsx
+++ App.tsx
@@ -438,7 +438,7 @@ const App: React.FC = () => {
               </div>
             </div>
           ) : !state.loading && (
-            <CenterMessage bigText={splashName} smallText="Whispers of Wisdom" />
+            <CenterMessage bigText={splashName} smallText="Wisdom Whispers" />
           )}
         </div>
       </main>
